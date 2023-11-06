7c0e7586d6ab225c17f36d30cda539973b884dd6
gemlorg <gosha10goshalol@gmail.com>
2023-11-06 12:00:55 +0100

    feat: add readme and results

---
+++ projects/logistic-regression/results/SUMMARY.md
@@ -0,0 +1,9 @@
+# Results
+
+## Visualisation 
+[logistic-regression/src/visual.ipynb](../src/visual.ipynb)
+
+## Issues
+ - Not enough info in  the dataset 
+ - Outdated dataset 
+ - Predicting the actual price might be more difficult
--- projects/logistic-regression/src/v2-logistic-regression.py
+++ projects/logistic-regression/src/v2-logistic-regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+import matplotlib.pyplot as plt
 import datetime
 
 
@@ -20,10 +21,11 @@ def timeseries_train_test_split(X, y, test_size):
 # let us try to predict the price in the next k weeks.
 
 
-def train_test_model(data, num_lags):
+def train_test_model(d, num_lags):
+    data = pd.DataFrame(d)
     for i in range(0, num_lags + 1):
         data["lag_{}".format(i)] = data.price.rolling(30).mean()
-        data["lag_{}".format(i)] = data["lag_{}".format(i)].shift(1 + 30 * i)
+        data["lag_{}".format(i)] = data["lag_{}".format(i)].shift(1 + 30 * (i+1))
 
     data = data.dropna()
     y = data["lag_0"] - data["lag_1"]
@@ -35,9 +37,52 @@ def train_test_model(data, num_lags):
     model = LogisticRegression(random_state=0)
     model.fit(X_train, y_train)
 
-    # prediction = model.predict(X_test, y_test)
-
     return model.score(X_test, y_test)
+
+def train_draw_model(d, num_lags, truedate):
+    data = pd.DataFrame(d)
+    for i in range(0, num_lags + 1):
+        data["lag_{}".format(i)] = data.price.rolling(30).mean()
+        data["lag_{}".format(i)] = data["lag_{}".format(i)].shift(1 + 30 * (i+1))
+
+    data = data.dropna()
+    y = data["lag_0"] - data["lag_1"]
+    y = y.apply(lambda x: (np.sign(x) + 1) // 2)
+    X = data.drop(["lag_0", "price"], axis=1)
+
+    X_train, X_test, y_train, y_test = timeseries_train_test_split(X, y, test_size=0.3)
+
+    model = LogisticRegression(random_state=0)
+    model.fit(X_train, y_train)
+
+    data['predicted_delta'] = model.predict(X)
+    data["truedate"] = truedate
+    # data["predicted_delta"].to_csv("./datapred.csv")
+
+
+    # Create a new column to store whether the prediction is correct
+    data['prediction_correct'] = data['predicted_delta'] == y
+
+    data = data.iloc[::30]
+
+    # Separate the data into correctly predicted and incorrectly predicted points
+    correct_predictions = data[data['prediction_correct']]
+    incorrect_predictions = data[~data['prediction_correct']]
+
+    
+
+    # Plot the correctly predicted points in green and incorrectly predicted points in red
+    
+    plt.scatter(correct_predictions['truedate'], correct_predictions['price'], c='green', label='Correct Predictions')
+    plt.scatter(incorrect_predictions['truedate'], incorrect_predictions['price'], c='red', label='Incorrect Predictions')
+
+    plt.xlabel('Date Sold')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.title('Logistic Regression Predictions For Monthly Average')
+
+    plt.savefig("../results/data/monthly-predictions-colored.png")
+
 
 
 # it would be harder to implement the model had we assumed
@@ -47,6 +92,7 @@ file_path = "../data/raw_sales.csv"
 data = pd.read_csv(file_path, parse_dates=["datesold"])
 data.sort_values(by="datesold", inplace=True)
 
+truedate = data["datesold"]
 data["datesold"] = data["datesold"].map(datetime.datetime.toordinal)
 
 
@@ -59,4 +105,5 @@ df = pd.DataFrame({"num_lags": [], "accuracy": []})
 for num_lags in range(1, 30):
     accuracy = train_test_model(data, num_lags)
     df.loc[len(df)] = [num_lags, accuracy]
+train_draw_model(data, 20, truedate)
 df.to_csv("../results/accuracies_monthly.csv")
