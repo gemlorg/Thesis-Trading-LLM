e1e3d4eb921ec0ce26920989a418452b03170d8e
ivgechu <72002104+ivgechu@users.noreply.github.com>
2023-11-17 22:56:56 +0100

    feat: change to PyTorch

--- projects/multilayer-perceptron/src/script.py
+++ projects/multilayer-perceptron/src/script.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 import datetime
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score
+import torch
+import torch.nn as nn
 
 data_path = os.path.join(os.path.dirname(__file__), "../data/raw_sales.csv")
+
+num_lags = 4
+in_features = num_lags + 1
+out_features = 1
 
 
 def add_lags_columns(data, num_lags, price_column):
@@ -17,7 +21,7 @@ def add_lags_columns(data, num_lags, price_column):
 
 def compute_price_deltas(data, price_column):
     data["price_delta"] = data[price_column] - data["lag_1"]
-    data["price_delta"] = np.sign(data["price_delta"])
+    data["price_delta"] = (np.sign(data["price_delta"]) + 1) // 2
     return data
 
 
@@ -29,7 +33,7 @@ def get_data(date_column="datesold", price_column="price"):
     columns_to_keep = [date_column, price_column]
     data = data[columns_to_keep]
 
-    data = add_lags_columns(data, 4, price_column)
+    data = add_lags_columns(data, num_lags, price_column)
     data = compute_price_deltas(data, price_column)
     data = data.drop([price_column], axis=1)
     return data
@@ -47,15 +51,27 @@ def split_data(data, test_size, target_column="price_delta"):
     X_test = data_test.drop([target_column], axis=1)
     y_test = data_test[target_column]
 
+    X_train = torch.tensor(X_train.values, dtype=torch.float32)
+    X_test = torch.tensor(X_test.values, dtype=torch.float32)
+    y_train = torch.tensor(y_train.values, dtype=torch.float32).reshape(-1, 1)
+    y_test = torch.tensor(y_test.values, dtype=torch.float32).reshape(-1, 1)
+
     return X_train, y_train, X_test, y_test
 
 
-def train_and_evaluate_mlp(mlp_model, X_train, y_train, X_test, y_test):
-    mlp_model.fit(X_train, y_train)
+def train_and_evaluate_mlp(
+    mlp_model, loss_fn, optimizer, num_epochs, X_train, y_train, X_test, y_test
+):
+    for n in range(num_epochs):
+        y_pred = mlp_model(X_train)
+        loss = loss_fn(y_pred, y_train)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    y_pred = mlp_model.predict(X_test)
+    y_pred = mlp_model(X_test)
 
-    accuracy = accuracy_score(y_test, y_pred)
+    accuracy = (y_pred.round() == y_test).float().mean().item()
     return accuracy
 
 
@@ -63,9 +79,19 @@ data = get_data()
 
 X_train, y_train, X_test, y_test = split_data(data, 0.3)
 
-mlp_model = MLPClassifier(
-    hidden_layer_sizes=(80), activation="logistic", random_state=1
+mlp_model = nn.Sequential(
+    nn.Linear(in_features, 30),
+    nn.ReLU(),
+    nn.Linear(30, 20),
+    nn.ReLU(),
+    nn.Linear(20, out_features),
+    nn.Sigmoid(),
 )
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.Adam(mlp_model.parameters(), lr=0.001)
+num_epochs = 1000
 
-accuracy = train_and_evaluate_mlp(mlp_model, X_train, y_train, X_test, y_test)
+accuracy = train_and_evaluate_mlp(
+    mlp_model, loss_fn, optimizer, num_epochs, X_train, y_train, X_test, y_test
+)
 print("accuracy =", accuracy)
